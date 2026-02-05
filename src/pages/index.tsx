f10609261a33e63d20aa087b5839f95b1ddca8e8
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-02-05 17:05:51 -0300

    feature: criação do componente de cabeçalho
    - Foi adicionado o Shadcn no projeto
    - Foi adicionado o componente Button do Shadcn
    - Foi criado o componente de cabeçalho

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
+import { Header } from '@/components/header'
+
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-zinc-800">
-      <h1 className="text-gray-200 text-2xl">Hello world</h1>
-    </div>
-  )
+  return <Header />
 }
