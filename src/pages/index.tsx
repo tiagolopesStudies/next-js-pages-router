9e82b3839091db753595e63313634998ced94fad
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-06-04 12:25:37 -0300

    feat: criação da seção de Features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from '@/components/base/hero-section'
+import { FeatureSection } from '@/components/feature-section'
 
 export default function HomePage() {
   return (
     <article className="flex flex-col">
       <HeroSection />
+      <FeatureSection />
     </article>
   )
 }
