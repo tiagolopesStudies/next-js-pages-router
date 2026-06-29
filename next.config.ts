0a7e49ea10fd1f56f2ceb49908c130d206313d6d
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-06-29 13:40:54 -0300

    chore: configurando a biblioteca ContentLayer

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from 'next'
+import { withContentlayer } from 'next-contentlayer'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
   reactStrictMode: true
 }
 
-export default nextConfig
+export default withContentlayer(nextConfig)
